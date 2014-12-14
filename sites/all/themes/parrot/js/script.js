36ce7373f2cb65e4720335a16ea28542129adb9b
graysadler <graysadler@gmail.com>
2014-12-14 15:58:10 -0500

    Add scroll to behavior on front page

--- sites/all/themes/parrot/js/script.js
+++ sites/all/themes/parrot/js/script.js
@@ -62,13 +62,6 @@
         $(".login-blocks #user-register-form .oneall_social_login > label,.login-blocks #user-login-form .oneall_social_login > label").hide();
         $(".login-blocks #user-register-form,.login-blocks #user-login-form").before("<div class='log-email'><strong>Join Us FREE!</strong> Sign up using social media or email:</div>");
 
-        $('.panels-flexible-region-7-sidebar_first-inside .social-share,.panels-flexible-region-7-sidebar_first-inside .view-header,.panels-flexible-region-7-sidebar_first-inside .item-list,.panels-flexible-region-7-sidebar_first-inside .watch-video').wrapAll('<div class="wrap">');
-        jQuery('.panels-flexible-region-7-sidebar_first-inside .wrap').scrollbar();
-        
-        jQuery('.panels-flexible-region-12-sidebar_first .streamriots-about,.panels-flexible-region-12-sidebar_first .social-share,.panels-flexible-region-12-sidebar_first .view-header,.panels-flexible-region-12-sidebar_first .item-list,.panels-flexible-region-8-sidebar_first-inside .social-share,.panels-flexible-region-8-sidebar_first-inside .view-header,.panels-flexible-region-8-sidebar_first-inside .item-list').wrapAll('<div class="wrap">');
-           jQuery('.panels-flexible-region-12-sidebar_first .wrap,.panels-flexible-region-8-sidebar_first-inside .wrap').scrollbar();
-        //  jQuery('.quick-accordion').scrollbar();
-
 //====================================================//
         var active = $("#quickset-help_center").find("a.active");
         if (active) {
@@ -89,39 +82,17 @@
 
         }
 
-       $('.sidebar-first').addClass('active-sidebar');
-
-          //fixedSidebar();
+        $('.home-video-inner .icon-video-text').click(function(e){
+          e.preventDefault();
+          var top = $('.join-us')[0].offsetTop; //Getting Y of target element
+          $('html, body').animate({
+            scrollTop: top
+          }, 1000);
+          //window.scrollTo(0, top); 
+        });
   
     });
-    
-    $(window).scroll(function(){
-    	//fixedSidebar();
-    });
-    
-    
-    function fixedSidebar()
-    { 
-       var footerOffset=$('#footer-wrapper').offset().top;
-       var footerHt=$('#footer-wrapper').height();
-       //console.log(footerHt);
-      
-       
-       if($(window).scrollTop() >= footerOffset/2.5+footerHt)
-       {
-       	       //console.log('if')
-       	$('.sidebar-first').removeClass('side-fixed');
-       }
-       else{ 
-       	       $('.sidebar-first').addClass('side-fixed');
-       	       //console.log('else');
-       }	
-    }
 
-//    Drupal.behaviors.exampleModule = {
-//    attach: function (context, settings) {
-//        
-//    }
-//   }
+
 })(jQuery);
 
